085a1e3f03778bd704254d892865dbedc4b851fa
debmoncho <elmetalerodelsur@gmail.com>
2024-03-20 11:02:12 -0300

    Modificacion
    Modificacion de diego

--- TpGrupal-Lab1/src/tpgrupal/lab1/TpGrupalLab1.java
+++ TpGrupal-Lab1/src/tpgrupal/lab1/TpGrupalLab1.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package tpgrupal.lab1;
 
 /**
@@ -14,7 +11,12 @@ public class TpGrupalLab1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        System.out.println("Holaa soy Diego");
+        
+        // el proximo sout puede ser eliminado o modificado
+        
+        System.out.println("Esto es una prueba....");
     }
     
 }
